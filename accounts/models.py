51ef440c82d0802a5735494e34d71e4f2496295c
Garrett Kucinski <garrettryan@doejo.com>
2018-02-10 09:24:23 -0700

    initial set up of form

---
+++ accounts/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+
+# Create your models here.
+
+
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+    confirm_email = models.EmailField(null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    bio = models.TextField(blank=True)
+    image = models.ImageField(blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, re_path
+
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('sign_in', views.sign_in, name='sign_in'),
+    path('sign_up', views.sign_up, name='sign_up'),
+    path('sign_out', views.sign_out, name='sign_out'),
+    path('view_profile', views.view_profile, name='view_profile'),
+    path('edit_profile', views.edit_profile, name='edit_profile'),
+]
